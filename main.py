6d47d0a3611b24226470a4ab2867edc0177cbc98
KirillYur <72611465+KirillYur@users.noreply.github.com>
2022-04-22 18:13:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import numpy as np
 import streamlit as st
 import pandas as pd
 from bokeh.plotting import figure
+from fpdf import FPDF
+import base64
+
 st.header("Курсовая работа Юричковский.К.А ТФэ-01-19 Вариант 23")
 st.write("Ссылка на код: https://github.com/KirillYur/Kirill_Yurichkovskij")
 st.header("Часть №1")
@@ -972,3 +975,20 @@ plt.grid(True)
 plt.plot(z, Hdi_, '-ro')
 plt.title('Рисунок 7. Распределение теплоперепадов с учетом невязки по проточной части')
 st.pyplot(fig)
+
+
+export_as_pdf = st.button("Export Report")
+
+def create_download_link(val, filename):
+    b64 = base64.b64encode(val)  # val looks like b'...'
+    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Download file</a>'
+
+if export_as_pdf:
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font('Arial', 'B', 16)
+    pdf.cell(40, 10, report_text)
+    
+    html = create_download_link(pdf.output(dest="S").encode("latin-1"), "test")
+
+    st.markdown(html, unsafe_allow_html=True)
